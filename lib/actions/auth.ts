38f37f2d3a6192a58b6905db2d41316ca2074b7e
Han <82151637@qq.com>
2025-08-04 23:46:20 +0800

    implement qstash workflows for user onboarding

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -8,6 +8,8 @@ import { db } from "@/database/drizzle";
 import { headers } from "next/headers";
 import ratelimit from "@/lib/ratelimit";
 import { redirect } from "next/navigation";
+import { workflowClient } from "@/lib/workflow";
+import config from "@/lib/config";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">,
@@ -64,6 +66,14 @@ export const signUp = async (params: AuthCredentials) => {
       email,
       universityId,
       universityCard,
+    });
+
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflow/onboarding`,
+      body: {
+        email,
+        fullName,
+      },
     });
 
     return { success: true };
